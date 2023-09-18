47c760f1280bc8523f52391fc55ef605cbdcf456
khadijaludhi <khadijaludhi@gmail.com>
2023-09-18 17:02:46 +0100

    Imported the random module & the string module

---
+++ password_generator.py
@@ -0,0 +1,5 @@
+import random # Imports the random module, which contains various functions for generating random numbers and making random choices. 
+# Used to randomly select characters to include in the password. 
+import string # Imports the string module, which contains a collection of string constants and string manipulation functions.
+# For example, string.ascii_letters is a string containing all the ASCII letters (lowercase and uppercase), and string.digits is a string containing all the digits from 0 to 9.
+
