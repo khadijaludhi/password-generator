46cb15e64b3a54997b5fc308172536e09fd2619b
khadijaludhi <khadijaludhi@gmail.com>
2023-09-18 19:04:45 +0100

    inserted pseudocode comments to help explain each section of the code.

--- password_generator.py
+++ password_generator.py
@@ -1,25 +1,31 @@
-import random # Imports the random module, which contains various functions for generating random numbers and making random choices. 
-# Used to randomly select characters to include in the password. 
-import string # Imports the string module, which contains a collection of string constants and string manipulation functions.
-# For example, string.ascii_letters is a string containing all the ASCII letters (lowercase and uppercase), and string.digits is a string containing all the digits from 0 to 9.
+import random # Used to randomly select characters to include in the password. 
+import string # Imports the string module for ASCII characters (lower & uppercase) and digits (0 to 9).
+
+# Pseudocode: Define a function named generate_password that takes three parameters:
+# - min_length: The minimum length of the generated password
+# - numbers: A flag to include numbers in the password or not
+# - special_characters: A flag to include special characters in the password or not
 
 def generate_password(min_length, numbers=True, special_characters=True):
-    letters = string.ascii_letters # Creates a variable letters that contains all lowercase and uppercase ASCII letters.
-    digits = string.digits # Creates a variable digits that contains all digits from 0 to 9.
-    special = string.punctuation # Creates a variable special that contains all special characters (like !, @, #, etc.).
-
-    # Base characters include the ASCII letters
+    # Initialize a variable with all ASCII letters
+    letters = string.ascii_letters 
+    # Initialize a variable with all digits
+    digits = string.digits 
+    # Initialize a variable with all special characters
+    special = string.punctuation 
+    
+    # Start with a character set that includes only ASCII letters
     characters = letters
-
-    # Add digits if numbers=True
+    
+    # # Pseudocode: If numbers should be included (if numbers=True), add digits to the character set.
     if numbers:
         characters += digits
-
-     # Add special characters if special_characters=True
+    
+    # Pseudocode: If special characters should be included (if special_characters=True), add special characters to the character set.
     if special_characters:
         characters += special
-
-    # Below are variables, and they're like little boxes where you can store information.
+    
+    # Initialize variables to hold the password and status flags
     pwd = "" # Initialises an empty string for the password 
     meets_criteria = False # This is a box named meets_criteria, and you're putting the value False in it. 
     # Later on, you'll check if the password meets certain conditions (like having numbers or special characters). If it does, you'll change this to True.
@@ -27,31 +33,40 @@ def generate_password(min_length, numbers=True, special_characters=True):
     # This is to keep track of whether your password has any numbers in it. If you later add a number to your password, you'll change this to True.
     has_special = False # This is another box, and it's just like has_number
     #  it keeps track of whether your password has any special characters (like !, @, #, etc.).
-    """ The loop below will keep running until two things are true:
-        1. meets_criteria is True, meaning your password meets all the conditions you've set (like having numbers or special characters).
-        2. The length of the password (pwd) is at least as long as min_length. """
-    while not meets_criteria or len(pwd) < min_length: 
+    
+    # Pseudocode: Loop until the password meets the criteria and reaches the minimum length
+    while not meets_criteria or len(pwd) < min_length:
+        # Pick a random character from the character set and add it to the password
         new_char = random.choice(characters)
         pwd += new_char
-        # These lines ^ pick a random character (new_char) from the characters list and then add it to the end of the password (pwd).
-        if new_char in digits: 
+
+        # Pseudocode: If the new character is a digit, set has_number to True
+        if new_char in digits:
             has_number = True
-        elif new_char in special: 
+
+        # Pseudocode: If the new character is a special character, set has_special to True
+        elif new_char in special:
             has_special = True
-        # Here, you're checking if the new character is a number or a special character. If it's a number, has_number becomes True. If it's a special character, has_special becomes True.
+
+        # Assume that the password now meets the criteria
         meets_criteria = True
+
+        # Pseudocode: If numbers are required, set meets_criteria based on has_number
         if numbers:
             meets_criteria = has_number
-        if special_characters: 
+
+        # Pseudocode: If special characters are required, adjust meets_criteria accordingly
+        if special_characters:
             meets_criteria = meets_criteria and has_special
-        """ Here, you're updating the meets_criteria flag. You start by assuming the password meets all criteria (True). Then you check:
-            1. If numbers are required, then meets_criteria is the same as has_number.
-            2. If special characters are also required, then meets_criteria needs both conditions to be True. """
+
+    # Return the generated password
     return pwd
 
-min_length = int(input("Enter the minimum length: "))  # Convert user input to integer
-has_number = input("Do you want to have numbers (y/n)? ").lower() == "y"  # Notice the parentheses after lower
-has_special = input("Do you want to have special characters (y/n)? ").lower() == "y"  # Same here
+# Pseudocode: Collect user inputs for minimum length, numbers, and special characters
+min_length = int(input("Enter the minimum length: "))  
+has_number = input("Do you want to have numbers (y/n)? ").lower() == "y"
+has_special = input("Do you want to have special characters (y/n)? ").lower() == "y"
 
-pwd = generate_password(min_length, has_number, has_special)  # Now min_length is an integer
+# Generate the password and print it
+pwd = generate_password(min_length, has_number, has_special)
 print("The generated password is:", pwd)
