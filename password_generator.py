f6761ea06e693c4331360ac2a0ed9c09973fa029
khadijaludhi <khadijaludhi@gmail.com>
2023-09-18 18:07:14 +0100

    variables = False

--- password_generator.py
+++ password_generator.py
@@ -3,3 +3,29 @@ import random # Imports the random module, which contains various functions for 
 import string # Imports the string module, which contains a collection of string constants and string manipulation functions.
 # For example, string.ascii_letters is a string containing all the ASCII letters (lowercase and uppercase), and string.digits is a string containing all the digits from 0 to 9.
 
+def generate_password(min_length, numbers=True, special_characters=True):
+    letters = string.ascii_letters # Creates a variable letters that contains all lowercase and uppercase ASCII letters.
+    digits = string.digits # Creates a variable digits that contains all digits from 0 to 9.
+    special = string.punctuation # Creates a variable special that contains all special characters (like !, @, #, etc.).
+
+    # Base characters include the ASCII letters
+    characters = letters
+
+    # Add digits if numbers=True
+    if numbers:
+        characters += digits
+
+     # Add special characters if special_characters=True
+    if special_characters
+        characters += special
+
+    # Below are variables, and they're like little boxes where you can store information.
+    pwd = "" # Initialises an empty string for the password 
+    meets_criteria = False # This is a box named meets_criteria, and you're putting the value False in it. 
+    # Later on, you'll check if the password meets certain conditions (like having numbers or special characters). If it does, you'll change this to True.
+    has_number = False # This is a box named has_number, and you're putting the value False in it. 
+    # This is to keep track of whether your password has any numbers in it. If you later add a number to your password, you'll change this to True.
+    has_special = False # This is another box, and it's just like has_number
+    #  it keeps track of whether your password has any special characters (like !, @, #, etc.).
+
+generate_password(10)
